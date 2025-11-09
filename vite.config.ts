fb9cf9a6b0efe6f62a447f648f470237dd6df251
PhoenixZeroph <109314075+PhoenixZeroph@users.noreply.github.com>
2025-11-09 20:23:02 -0300

    Update vite.config.ts
    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173
-  }
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    define: {
+      __CONTRACT_ADDRESS__: JSON.stringify(env.VITE_CONTRACT_ADDRESS),
+      __CHAIN_ID__: JSON.stringify(env.VITE_CHAIN_ID),
+      __NGROK_URL__: JSON.stringify(env.VITE_NGROK_URL),
+      __BASE_RPC__: JSON.stringify(env.VITE_BASE_RPC_URL)
+    },
+    server: {
+      port: 5173,
+      strictPort: true
+    }
+  };
 });
