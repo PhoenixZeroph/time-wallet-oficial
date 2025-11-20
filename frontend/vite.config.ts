4c9920c9943cfcd4cc1ab1a0f03fc043537aceff
PhoenixZeroph <109314075+PhoenixZeroph@users.noreply.github.com>
2025-11-20 05:52:15 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite'
 import litcss from 'vite-plugin-lit-css'
 export default defineConfig({ plugins:[litcss()] })
+import { defineConfig } from 'vite'
+import litcss from 'vite-plugin-lit-css'
+
+export default defineConfig({
+  /** <-- 🔑  añade esta línea */
+  root: 'public',
+  build: {
+    outDir: '../dist',     // dist sigue quedando al nivel de frontend/
+    emptyOutDir: true
+  },
+  plugins: [litcss()]
+})
