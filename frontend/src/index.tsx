e3729e2ca68ca1d225251fabd6e25b64d80a1082
PhoenixZeroph <109314075+PhoenixZeroph@users.noreply.github.com>
2025-11-25 06:26:57 -0300

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import TimeWalletApp from "./TimeWalletApp.tsx"; // tu componente principal
+import TimeWalletApp from "./TimeWalletApp";
 
-const container = document.getElementById("root") as HTMLElement;
+const container = document.getElementById("app")!;
 createRoot(container).render(
   <React.StrictMode>
     <TimeWalletApp />
