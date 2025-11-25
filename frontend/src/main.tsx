216771fe727ffa8877f79a683248937854c5026c
PhoenixZeroph <109314075+PhoenixZeroph@users.noreply.github.com>
2025-11-25 16:03:56 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TimeWalletApp from "./TimeWalletApp";
+import App from "./App";
 import "./index.css";          // tailwind
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TimeWalletApp />
+    <App />
   </React.StrictMode>
 );
