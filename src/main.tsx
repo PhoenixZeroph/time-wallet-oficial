97462617ac336fca6100ee6c85ba896cb4b85b7b
PhoenixZeroph <109314075+PhoenixZeroph@users.noreply.github.com>
2025-11-25 04:33:31 -0300

    Update main.tsx
    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { LemonMiniApp } from '@lemoncash/mini-app-sdk';
 import App from './App';
+import TimeWalletApp from "./TimeWalletApp";
+import "./index.css";          // tailwind
 
 LemonMiniApp.ready();
 
